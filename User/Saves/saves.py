feed63b36ab4c3989bc127655eeb65c08cfbf28a
tremi <tremisabdoul@gmail.com>
2021-02-12 13:35:23 +0100

    savestart

---
+++ User/Saves/saves.py
@@ -0,0 +1,62 @@
+
+#Slots de save:
+
+"""
+Liste d'infos dans les saves:
+
+    1:
+    2:
+    3:
+    4:
+    5:
+    6:
+    7:
+    8:
+    9:
+    10:
+    ...
+
+"""
+
+class SaveSlot():
+    def __init__(self):
+        self.Save1 = {
+
+        "NameSave": "Number 1",
+	"NamePlayer": "Base",
+	
+
+        # Player
+
+        # Statistiques Player -tremisabdoul
+        "Game.Player.Pv" : 50,
+        "Game.Player.MaxPv" : 100,
+        "Game.Player.Damage" : 10,
+        "Game.Player.Speed" : 3,
+        "Game.Player.SpeedY" : 0,
+        "Game.Player.Level" : 0,
+        "Game.Player.Gold" : 0,
+
+        # Position de Player -tremisabdoul
+        "Game.Player.rect.x" : 50,
+        "Game.Player.rect.y" : 82,
+        "Game.Player.LastY" : 0,
+        "Game.Player.YVector" : 0,
+
+
+        # Force
+
+        #Mouvement Actuel de Player -tremisabdoul
+        "Game.Force.lastx" : float(0),
+        "Game.Force.Base_Gravity" : 0,
+
+
+        }
+        self.Save2 = {}
+        self.Save3 = {}
+        self.Save4 = {}
+        self.Save5 = {}
+        self.Save6 = {}
+        self.Save7 = {}
+        self.Save8 = {}
+        self.Save9 = {}
